3af16b6fc68738f348f51250e48f45b7c6573e0e
Joao Pedro <joaopedrobeckland@gmail.com>
2019-10-26 11:22:05 -0300

    First Commit

---
+++ src/routes.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+  createAppContainer,
+  createSwitchNavigator,
+  createBottomTabNavigator,
+  createStackNavigator,
+} from 'react-navigation';
+
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
+
+export default (isSigned = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({
+          SignIn,
+          SignUp,
+        }),
+        App: createBottomTabNavigator(
+          {
+            Dashboard,
+            Profile,
+          },
+          {
+            tabBarOptions: {
+              resetOnBlur: true,
+              keyboardHidesTabBar: true,
+              activeTintColor: '#FFF',
+              inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
+              style: {
+                backgroundColor: '#402845',
+              },
+            },
+          }
+        ),
+      },
+      {
+        initialRouteName: isSigned ? 'App' : 'Sign',
+      }
+    )
+  );
